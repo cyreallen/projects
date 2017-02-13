0100c0bca488be0a912376ded0ac8c2598708a76
Cyre Spencer <cyre.s.allen@gmail.com>
2017-02-12 21:29:18 -0500

    Zombie madness js and html files

--- .idea/Zombie.js
+++ .idea/Zombie.js
@@ -1,3 +1,35 @@
 /**
  * Created by cyresallen on 2/12/17.
  */
+
+
+
+var zombies = 1;
+var bites = 4;
+var days = 8;
+var newZoms;
+
+for(i = 0; i <= days.length; i++){
+
+    zombies * bites;
+
+    zombie += newZoms;
+
+    console.log("On day " + i + "there is/are " + zombies + "zombies");
+
+
+}
+
+
+var numDays = 1;
+var horde = 1;
+
+while(horde <= 320000000) {
+
+    var infected = horde * bites;
+    horde += infected;
+
+    console.log("Day number " + numDays + horde + "zombies");
+    numDays++;
+}
+
