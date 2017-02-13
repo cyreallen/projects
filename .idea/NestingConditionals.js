48846a5fb3f4ad2d859ad98d2699d0aa9e7e86da
Cyre Spencer <cyre.s.allen@gmail.com>
2017-02-12 21:55:26 -0500

    Javascript files for nesting conditionals

---
+++ .idea/NestingConditionals.js
@@ -0,0 +1,22 @@
+/**
+ * Created by cyresallen on 2/12/17.
+ */
+
+var temp = 67;
+var waterTemp = 90;
+
+if(temp >= 85) {
+
+    console.log("It is warm enough, lets go to the beach!");
+
+    if(waterTemp >= 75){
+
+        console.log("let's go swimming!");
+    } else {
+
+        console.log("water is a little cold, lets get a tan");
+    }
+}else {
+
+    console.log("It's cold, so lets go see a movie.");
+}
